2d2d787f173a97c1ab4f87c392f5db1eb1197588
Sumayyah Asgar <sasgar04@gmail.com>
2018-12-18 16:28:57 -0800

    Sidebar wip 🍫

--- intro/src/App.vue
+++ intro/src/App.vue
@@ -1,36 +1,57 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-    <Notes />
+    <div class='wrapper'>
+      <SideBar />
+      <Notes class='right-side' />
+    </div>
   </div>
 </template>
 
 <script>
-//import HelloWorld from './components/HelloWorld.vue'
-import Notes from './components/Notes.vue'
+  //import HelloWorld from './components/HelloWorld.vue'
+  import Notes from './components/Notes.vue'
+  import SideBar from './components/SideBar.vue'
 
-export default {
-  name: 'app',
-  components: {
-    Notes
+  export default {
+    name: 'app',
+    components: {
+      Notes,
+      SideBar
+    }
   }
-}
 </script>
 
-  <!-- 
+<!-- 
   name of the script would be the componet you are exporting
   data is similar to state in react
   reset the state by setting todo to an empty string every time after you add a new todo
   -->
 
-<style>-
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style>
+  #app {
+    width: 100%;
+   max-width: 1000px;
+   min-height: 100vh;
+   margin: 0 auto;
+    text-align: center;
+  }
+
+  .wrapper {
+    width: 100%;
+    display: flex;
+    @media (max-width: 500px) {
+      flex-direction: column;
+    }
+
+    .right-side {
+      width: 75%;
+      min-height: 100vh;
+      background-color: #f3f3f3;
+      @media (max-width: 500px) {
+        width: 100%
+      }
+    }
+  }
 </style>
---
+++ intro/src/components/SideBar.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class='container'>
+    <h2>Lambda Notes</h2>
+    <button>View Your Notes</button>
+    <button>Create a New Note</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'SideBar',
+  }
+</script>
+
+<style scoped>
+.container {
+   width: 25%;
+  height: auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #d8d8d8;
+   @media (max-width: 500px) {
+    width: 100%
+  }
+
+  button {
+  width: 100%;
+  height: 60px;
+  margin-bottom: 10%;
+  background-color: #dea6af;
+  color: white;
+  text-align: center;
+  &:hover {
+    cursor: pointer;
+  };
+  }
+}
+
+</style>
